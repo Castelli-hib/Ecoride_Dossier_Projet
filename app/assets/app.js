ff845bbb1e39fd5d7ab0df04daf072023e3702cd
Castelli-hib <marie.castelli@adiktionstudio.com>
2025-12-15 17:46:15 +0100

    Remove nested app folder and ignore it

--- app/assets/app.js
+++
@@ -1,7 +0,0 @@
-// Import du CSS/SCSS principal
-import './styles/app.scss';
-
-
-// (optionnel) Import de Bootstrap JS si tu veux les composants interactifs
-import * as bootstrap from 'bootstrap';
-
